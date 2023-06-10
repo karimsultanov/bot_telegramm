8f0fa095aa96f559b6fd2e10540472e6c50e8290
Kerimchick <47135315+Kerimchick@users.noreply.github.com>
2023-06-10 07:02:28 +0300

    fix time zone

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = '6172539606:AAEwNdxt5lH2PNospcFl0JfvqY0QyIdCB7w'
 const bot = new TelegrammApi(token, {polling: true})
 bot.on('message', msg => {
     const chatId = msg.chat.id
-    cron.schedule('0 7 * * *',  () => {
+    cron.schedule('10 7 * * *',  () => {
         bot.sendMessage(chatId, 'Здравствуйте товарищи самураи,хищники,гангстеры,муковозы и просто водители пора делать бабки')
     }, {
         scheduled: true,
